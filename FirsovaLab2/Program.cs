7c3a84017bc89b57846174a3e8c178dfb246f9ec
Firsoova08 <foxalishca@mail.ru>
2025-09-22 12:56:35 +0300

    aa

--- FirsovaLab2/Program.cs
+++ FirsovaLab2/Program.cs
@@ -5,6 +5,133 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //double a = 10;
+            //double b=3;
+            //double c = a/b;
+            //Console.WriteLine($"Переменная с = {c}");
+            //double x = 10 / 3;
+            //Console.WriteLine($"Переменная x = {x}");
+
+
+            //int x1 = 4;
+            //int y1 = ++x1;
+            //Console.WriteLine($"x1 = {x1}, y1 = {y1}");
+
+            //int x2 = 4;
+            //int y2 = x2++;
+            //Console.WriteLine($"x2 = {x2}, y2 = {y2}");
+
+            //int x = 10 / 5 * 2; 
+            //Console.WriteLine(x); //сначала идет деление, затем умножение
+
+            //double a = 0.1;
+            //double b = 0.2;
+            //double c= a + b;
+            //double d = 0.3;
+            //Console.WriteLine(c == d);
+
+            //decimal x = 0.1m;
+            //decimal y = 0.2m;
+            //decimal z=x+ y;
+            //decimal e = 0.3m;
+            //Console.WriteLine(e == z);
+
+            //int a = 25;
+            //int b = 17;
+            //Console.WriteLine(a == b);
+
+            //int a = 45;
+            //int b = 13;
+            //Console.WriteLine(a != b);
+            //Console.WriteLine(a != 45);
+
+            //int a = 45;
+            //int b = 13;
+            //Console.WriteLine(a < b);
+
+            //int a = 45;
+            //int b = 13;
+            //Console.WriteLine(a > b);
+
+            //int a = 45;
+            //int b = 13;
+            //Console.WriteLine(a <= b);
+
+            //int a = 45;
+            //int b = 13;
+            //Console.WriteLine(a >= b);
+
+            //bool x1 = (5 > 6) & (4< 6);
+            //bool x2 = (5 < 6) & (4 < 6);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+            //bool x1 = (5 > 6) | (4 < 6);
+            //bool x2 = (5 < 6) | (4 < 6);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+            //bool x1 = (5 > 6) && (4 < 6);
+            //bool x2 = (5 < 6) && (4 < 6);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+            //bool x1 = (5 > 6) || (4 < 6);
+            //bool x2 = (5 < 6) || (4 < 6);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+
+            //bool a=true;
+            //bool b = !a;
+            //Console.WriteLine(b);
+
+            //bool x1 = (5 > 6) ^ (4 < 6);
+            //bool x2 = (50 > 6) ^ (4 / 2 < 3);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+
+            //double number = 1234.56789;
+            //Console.WriteLine("{0:F2}", number);
+            //Console.WriteLine("{0:C}",number);
+            //Console.WriteLine("{0:P}", 0.1234);
+
+            //Console.Write("Введите целое число: ");
+            //string? input = Console.ReadLine();
+            //int number = Convert.ToInt32(input);
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("Число положительное.");
+            //}
+            //else if (number < 0)
+            //{
+            //    Console.WriteLine("Число отрицательное.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Это ноль.");
+            //}
+            //int x = 3;
+            //int y = 2;
+            //int z=x<y? (x+y): (x-y);
+            //Console.WriteLine(z);
+
+            //string game = Console.ReadLine().ToLower();
+            //switch (game)
+            //{
+            //    case "Dota 2":
+            //        Console.WriteLine("Ваша игра - Dota 2");
+            //        break;
+            //    case "CS":
+            //        Console.WriteLine("Ваша игра - Counter-Strike"); break;
+            //    case "Minecraft":
+            //        Console.WriteLine("Ваша игра - Minecraft");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Игра не найдена");
+            //        break;
+
+            //}
         }
+
+
     }
+    
 }
---
+++ NewProgram/Program.cs
@@ -0,0 +1,2 @@
+﻿
+Console.WriteLine(Environment.OSVersion.VersionString);
